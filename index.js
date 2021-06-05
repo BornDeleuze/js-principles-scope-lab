22aefc3e07710a7f7afbc90e7ed06621d5bed7ab
Matt Beaudoin <ehxtwo@gmail.com>
2021-06-04 18:48:44 -0700

    commit tree b8b226fe85795f4b3ffc9300b4803976e5620239

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+   
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob';
+    console.log(bestCustomer);
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    console.log(bestCustomer);
+}
+const leastFavoriteCustomer = "Joe"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "bob"
+}
